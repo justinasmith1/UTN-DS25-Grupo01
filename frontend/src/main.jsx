6e3ff7942abbea931cbf6969389320d681460ae5
Santiago Talavera <santiago.a.talavera@gmail.com>
2025-09-13 23:32:39 -0300

    Provider de toasts y estados loading/empty; reemplazo alerts por toasts en Layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import AuthProvider from "./app/providers/AuthProvider"
+import ToastProvider from "./app/providers/ToastProvider.jsx";
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ToastProvider />
+          <App />
+        <ToastProvider />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
