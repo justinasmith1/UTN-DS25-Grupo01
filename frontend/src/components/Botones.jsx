b8b018e70ea4f1a799498e86ace588963d296089
Santiago Talavera <santiago.a.talavera@gmail.com>
2025-09-14 01:08:06 -0300

    Frontend modulo inmobiliarias funcionando

--- frontend/src/components/Botones.jsx
+++ frontend/src/components/Botones.jsx
@@ -24,10 +24,10 @@ export default function Botones() {
 
   // Defino los módulos con su permiso y (si aplica) a dónde navega
   const modules = [
-    { label: "Ventas",         perm: PERMISSIONS.SALE_ACCESS,    to: "/ventas" }, // (cuando haya ruta: to: "/ventas")
-    { label: "Inmobiliarias",  perm: PERMISSIONS.AGENCY_ACCESS,  to: null }, // to: "/inmobiliarias"
+    { label: "Ventas",         perm: PERMISSIONS.SALE_ACCESS,    to: "/ventas" }, 
+    { label: "Inmobiliarias",  perm: PERMISSIONS.AGENCY_ACCESS,  to: "/inmobiliarias" },
     { label: "Personas",       perm: PERMISSIONS.PEOPLE_ACCESS,  to: null }, // to: "/personas"
-    { label: "Reservas",       perm: PERMISSIONS.RES_ACCESS,     to: "/reservas" }, // to: "/reservas"
+    { label: "Reservas",       perm: PERMISSIONS.RES_ACCESS,     to: "/reservas" }, 
     { label: "Reportes",       perm: PERMISSIONS.REPORTS_ACCESS, to: null }, // to: "/reportes"
   ];
 
